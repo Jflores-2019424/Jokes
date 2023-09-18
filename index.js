60a0b7399bdcacb4809e7e95dfb5d0002646f50f
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-09-18 07:42:00 -0600

    Mas chistes

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const jokes = [
      "The best time on a clock is 6:30--hands down.",
      "Did you hear about the two thieves who stole a calendar? They each got six months.",
      "I cut my finger chopping cheese, but I think that I may have grater problems.",
-     "What did the piece of bread say to the knife? Butter me up"
+     "What did the piece of bread say to the knife? Butter me up",
+     "I knew I shouldn't steal a mixer from work, but it was a whisk I was willing to take.",
+     "When people are sad, I sometimes let them colour in my tattoos. Sometimes all they need is a shoulder to crayon.",
+     "I'm glad I know sign language, it's pretty handy.",
+     "What do you get hanging from Apple trees? Sore arms."
 ]
 
 btnJoke.addEventListener('click', generateJoke)
